2d88087fd40f3549cf74a597fde13fa07d9e45a9
Dai Bingzhi <daibingzhi9519@outlook.com>
2019-09-12 17:22:25 +0800

    add NeedPrimaryKey Exception

--- gogexceptions.py
+++ gogexceptions.py
@@ -1,26 +1,35 @@
 from aiohttp.client_exceptions import ClientResponseError
 
+
 class GOGBaseException(Exception):
     pass
 
 
-class GOGBadRequest(GOGBaseException):
+class GOGAPIException(GOGBaseException):
+    pass
+
+
+class GOGDatabaseException(GOGBaseException):
+    pass
+
+
+class GOGBadRequest(GOGAPIException):
     def __init__(self):
         super().__init__('Bad Request, please check url or payload')
 
 
-class GOGNetworkError(GOGBaseException):
+class GOGNetworkError(GOGAPIException):
     def __init__(self):
         super().__init__('Network Error, try again later')
 
 
-class GOGNotFound(GOGBaseException):
+class GOGNotFound(GOGAPIException):
 
     def __init__(self):
         super().__init__("Page Not Fund")
 
 
-class GOGResponseError(GOGBaseException):
+class GOGResponseError(GOGAPIException):
 
     @classmethod
     def judge_status(cls, exp: ClientResponseError):
@@ -30,19 +39,19 @@ class GOGResponseError(GOGBaseException):
             return GOGNetworkError()
 
 
-class GOGProductNotFound(GOGBaseException):
+class GOGProductNotFound(GOGAPIException):
     def __init__(self, prod_id):
         self.product_id = prod_id
         super().__init__(f'Product {prod_id} not exists')
 
 
-class GOGUnknowError(GOGBaseException):
+class GOGUnknowError(GOGAPIException):
 
     def __init__(self, exp_instance: Exception):
         super().__init__(f"error type: {type(exp_instance)}\nerror message: {str(exp_instance)}")
 
 
-class GOGLoginError(GOGBaseException):
+class GOGLoginError(GOGAPIException):
     pass
 
 
@@ -54,3 +63,7 @@ class GOGNeedVerification(GOGLoginError):
 class GOGAccountError(GOGLoginError):
     def __init__(self):
         super().__init__('GOG Username or Password Error')
+
+
+class NeedPrimaryKey(GOGDatabaseException):
+    pass
